78145231b4ea2b0c10ff4a4073f4f41e9f3d722a
Fernando Rojo <fernandosrojo@yahoo.com>
2019-08-25 13:01:04 -0400

    update typescript

--- src/FuegoContext/types.ts
+++ src/FuegoContext/types.ts
@@ -1,11 +1,12 @@
-import { FirestoreDbType } from '../Fuego/types';
-import firebase from 'firebase';
+import { FirestoreDbType } from '../Fuego/types'
+import firebase from 'firebase'
 
 export interface FuegoContextProps {
-	db: FirestoreDbType;
-	auth: firebase.auth.Auth;
-	addListener: (name: string, listener: () => void) => void;
-	removeListener: (name: string) => void;
-	doesListenerExist: (name: string) => boolean;
-	getListener: (name: string) => (() => void) | null;
+  db: FirestoreDbType
+  auth: firebase.auth.Auth
+  firebase: any
+  addListener: (name: string, listener: () => void) => void
+  removeListener: (name: string) => void
+  doesListenerExist: (name: string) => boolean
+  getListener: (name: string) => (() => void) | null
 }
--- src/hooks/useFuegoContext/index.ts
+++ src/hooks/useFuegoContext/index.ts
@@ -1,11 +1,12 @@
 import { useContext } from 'react'
 import FuegoContext from '../../FuegoContext'
+import { FuegoContextProps } from '../../FuegoContext/types'
 
 export default () => {
   // const { db, auth, addListener, removeListener, doesListenerExist, getListener } = useContext(
   // 	FuegoContext,
   // );
   // return { db, auth, addListener, removeListener, doesListenerExist, getListener };
-  const context = useContext(FuegoContext)
+  const context: FuegoContextProps = useContext(FuegoContext)
   return context
 }
