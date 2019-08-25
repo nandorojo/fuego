53e10f774a59ce2c2f351610e73f4ba1d3bd3124
Fernando Rojo <fernandosrojo@yahoo.com>
2019-08-25 13:45:55 -0400

    update firebase.auth type

--- src/Fuego/index.ts
+++ src/Fuego/index.ts
@@ -5,14 +5,14 @@ import { FuegoConfig } from './types'
 
 export default class Fuego {
   public db: firebase.firestore.Firestore
-  public auth: firebase.auth.Auth
+  public auth: typeof firebase.auth
   public firebase: typeof firebase
 
   constructor(config: FuegoConfig) {
     this.db = !firebase.apps.length
       ? firebase.initializeApp(config).firestore()
       : firebase.app().firestore()
-    this.auth = firebase.auth()
+    this.auth = firebase.auth
     this.firebase = firebase
   }
 }
