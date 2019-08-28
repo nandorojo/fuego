be2bc1385754f2478519333d9189334044696373
Fernando Rojo <fernandosrojo@yahoo.com>
2019-08-28 10:53:55 -0400

    export gates

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ import GetFuego from './components/GetFuego/index'
 import Fuego from './Fuego/index'
 import FuegoQuery from './FuegoQuery/index'
 export { default as useFuegoContext } from './hooks/useFuegoContext'
+export { default as FuegoGate } from './components/FuegoGate'
+export { default as connectFuegoGate } from './HOC/connectFuegoGate'
 
 export { connectFuego, useFuego, FuegoProvider, GetFuego, Fuego, FuegoQuery }
