8243cc36c9225189f54eddcfa4abc95e30469a21
Fernando Rojo <fernandosrojo@yahoo.com>
2019-08-30 14:35:03 -0400

    update config appId type

--- src/Fuego/types.ts
+++ src/Fuego/types.ts
@@ -1,15 +1,15 @@
 export interface FuegoConfig {
-	apiKey: string
-	authDomain: string
-	databaseURL: string
-	projectId: string
-	storageBucket: string
-	messagingSenderId: string
-	appId: string
+  apiKey: string
+  authDomain: string
+  databaseURL: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId?: string
 }
 
 export type FirestoreDbType = firebase.firestore.Firestore
 
 export interface FuegoType {
-	db: firebase.firestore.Firestore
+  db: firebase.firestore.Firestore
 }
